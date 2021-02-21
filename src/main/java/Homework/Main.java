fa7c4eba29f7e96e4387dc80a0e547ac6b9d967c
SergTaranenko <rates-13@inbox.ru>
2021-02-21 23:08:48 +0300

    1

--- src/main/java/Homework/Main.java
+++ src/main/java/Homework/Main.java
@@ -1,4 +1,41 @@
 package Homework;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class Main {
+
+    public static final int CARS_COUNT = 4;
+    public static final CyclicBarrier BARRIER = new CyclicBarrier(CARS_COUNT + 1);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(75), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        try {
+
+            while (BARRIER.getNumberWaiting() != CARS_COUNT) {
+                Thread.sleep(200);
+            }
+
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            BARRIER.await();
+
+            while (BARRIER.getNumberWaiting() != CARS_COUNT) {
+                Thread.sleep(200);
+            }
+
+            BARRIER.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (BrokenBarrierException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
